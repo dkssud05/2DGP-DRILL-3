807fd7cb463a3135139d2c34d47caca9864612b6
dkssud05 <hyeonuson51@gmail.com>
2025-09-17 21:08:30 +0900

    삼각형 운동 개발

--- character_moves.py
+++ character_moves.py
@@ -12,9 +12,28 @@ angle = 0
 r = 200
 circle_x = 400
 circle_y = 300
+triangle_state = 0
 
 def move_triangle():
-    pass
+    global x, y, triangle_state
+
+    if triangle_state == 0:
+        if x < 600 and y < 400:
+            x += 2
+            y += 3
+        else:
+            triangle_state = 1
+    elif triangle_state == 1:
+        if x > 200 and y < 400:
+            x -= 2
+        else:
+            triangle_state = 2
+    elif triangle_state == 2:
+        if x < 400 and y > 90:
+            x += 2
+            y -= 3
+        else:
+            triangle_state = 0
 
 def move_rectangle():
     global x, y
@@ -33,14 +52,13 @@ def move_circle():
     x = circle_x + r * math.sin(math.radians(angle))
     y = circle_y + r * math.cos(math.radians(angle))
     angle += 2
-    pass
 
 while True:
     clear_canvas_now()
     grass.draw_now(400, 30)
     character.draw_now(x, y)
 
-    move_circle()
+    move_triangle()
 
     delay(0.01)
 
