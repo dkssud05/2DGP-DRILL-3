ec1d49a650f79f39255feafad7dc4c741397375e
dkssud05 <hyeonuson51@gmail.com>
2025-09-17 21:01:07 +0900

    사각형 운동 개발

--- character_moves.py
+++ character_moves.py
@@ -10,27 +10,28 @@ x = 400
 y = 90
 
 def move_triangle():
-    print("move_triangle")
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, y)
     pass
 
 def move_rectangle():
-    print("move_rectangle")
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, y)
-    pass
+    global x, y
+
+    if x < 780 and y == 90:
+        x += 2
+    elif x == 780 and y < 550:
+        y += 2
+    elif x > 20 and y == 550:
+        x -= 2
+    elif x == 20 and y > 90:
+        y -= 2
 
 def move_circle():
-    print("move_circle")
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, y)
     pass
 
 while True:
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x, y)
+
     move_triangle()
     move_rectangle()
     move_circle()
