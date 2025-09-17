70b297bc3ad7c5b8d70db6d8fc1888481d19d2a0
dkssud05 <hyeonuson51@gmail.com>
2025-09-17 15:50:28 +0900

    함수 만들기

---
+++ character_moves.py
@@ -0,0 +1,32 @@
+from pico2d import *
+import math
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+x = 400
+y = 90
+
+def move_triangle():
+    pass
+
+def move_rectangle():
+    pass
+
+def move_circle():
+    pass
+
+while True:
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x, y)
+
+    move_triangle()
+    move_rectangle()
+    move_circle()
+
+    delay(0.001)
+
+close_canvas()
