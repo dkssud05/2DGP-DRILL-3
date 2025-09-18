5cb4fe3b01286ed7913e504574c221b1c3fe5d4b
dkssud05 <hyeonuson51@gmail.com>
2025-09-18 17:16:13 +0900

    state 변수 추가

--- character_moves.py
+++ character_moves.py
@@ -13,9 +13,25 @@ r = 200
 circle_x = 400
 circle_y = 300
 triangle_state = 0
+state = 0
+
+def move_rectangle():
+    global x, y, state
+
+    if x < 780 and y == 90:
+        x += 2
+        if x == 398:
+            x = 400
+            state = 1
+    elif x == 780 and y < 550:
+        y += 2
+    elif x > 20 and y == 550:
+        x -= 2
+    elif x == 20 and y > 90:
+        y -= 2
 
 def move_triangle():
-    global x, y, triangle_state
+    global x, y, triangle_state, state
 
     if triangle_state == 0:
         if x < 600 and y < 400:
@@ -34,31 +50,30 @@ def move_triangle():
             y -= 3
         else:
             triangle_state = 0
-
-def move_rectangle():
-    global x, y
-
-    if x < 780 and y == 90:
-        x += 2
-    elif x == 780 and y < 550:
-        y += 2
-    elif x > 20 and y == 550:
-        x -= 2
-    elif x == 20 and y > 90:
-        y -= 2
+            x = 400
+            y = 90
+            state = 1
 
 def move_circle():
-    global x, y, r, angle
+    global x, y, r, angle, state
     x = circle_x + r * math.sin(math.radians(angle))
     y = circle_y + r * math.cos(math.radians(angle))
     angle += 2
+
+    if angle > 360:
+        angle = 0
+        x = 400
+        y = 90
+        state = 0
 
 while True:
     clear_canvas_now()
     grass.draw_now(400, 30)
     character.draw_now(x, y)
 
+    move_rectangle()
     move_triangle()
+    move_circle()
 
     delay(0.01)
 
